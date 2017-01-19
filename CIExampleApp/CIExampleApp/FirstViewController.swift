0ea25d398885aa500c1e8fc92a519c402652437f
Kiel Oleson <kiel@jusquici.org>
2017-01-19 15:41:12 -0800

    update for Xcode 8.2 (breaking change)

---
+++ CIExampleApp/CIExampleApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CIExampleApp
+//
+//  Created by Kiel Oleson on 1/19/17.
+//  Copyright © 2017 Kiel Oleson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
